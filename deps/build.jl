8ffe4c87afc0a443528dde059f4a81831308a395
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-11-23 23:38:10 +0900

    Try OS X support
    Permutahedron tests fail with InexactError()

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,8 @@ using BinDeps
 
 @BinDeps.setup
 
-lrslib_commit = "2b35cc36c39d1ebca35dbc21a75a35864f0c07b1"
+#lrslib_commit = "2b35cc36c39d1ebca35dbc21a75a35864f0c07b1"
+lrslib_commit = "4bb57b5aac78deb1e9a25edf81bea16f5611596c"
 #lrsname = "lrslib-061"
 lrsname = "lrslib-$lrslib_commit"
 
@@ -16,7 +17,8 @@ lrsname = "lrslib-$lrslib_commit"
 liblrs = library_dependency("liblrs", aliases=[lrsname, "liblrsgmp"])#, depends=[libgmpdev])
 
 official_repo = "http://cgm.cs.mcgill.ca/~avis/C/lrslib/archive/$lrsname.tar.gz"
-forked_repo = "https://github.com/blegat/lrslib/archive/$lrslib_commit.zip"
+#forked_repo = "https://github.com/blegat/lrslib/archive/$lrslib_commit.zip"
+forked_repo = "https://github.com/oyamad/lrslib/archive/$lrslib_commit.zip"
 
 #GMP
 @linux_only begin
@@ -48,6 +50,11 @@ lrslibdir = joinpath(lrsprefixdir, "lib")
 
 targetdirs = AbstractString["liblrsgmp.$(Libdl.dlext)"]
 
+MAKEFILE = "makefile"
+@static if is_apple()
+    MAKEFILE = "makefile.osx"
+end
+
 provides(BuildProcess,
 	(@build_steps begin
 		GetSources(liblrs)
@@ -56,7 +63,7 @@ provides(BuildProcess,
 		@build_steps begin
 			ChangeDirectory(lrssrcdir)
 			FileRule(joinpath(lrslibdir,"liblrsgmp.$(Libdl.dlext)"),@build_steps begin
-				`make all-shared`
+				`make all-shared --file=$MAKEFILE SONAME=liblrsgmp.$(Libdl.dlext).0 SHLINK=liblrsgmp.$(Libdl.dlext)`
 				`cp liblrsgmp.$(Libdl.dlext) $lrslibdir/liblrsgmp.$(Libdl.dlext)`
 			end)
 		end
