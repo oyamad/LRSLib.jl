f45f799aa199d18361064c6086ffb0f4f1bcf229
Benoît Legat <benoit.legat@gmail.com>
2017-05-22 21:54:00 +0200

    Add basicpolyhedrontests

--- test/polyhedron.jl
+++ test/polyhedron.jl
@@ -1,6 +1,7 @@
 const polyhedra_test = joinpath(Pkg.dir("Polyhedra"), "test")
 
 include(joinpath(polyhedra_test, "alltests.jl"))
+basicpolyhedrontests(LRSLibrary())
 @testset "Polyhedra tests" begin
     runtests(LRSLibrary())
 end
